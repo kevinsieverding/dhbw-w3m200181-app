c20b437724c71ffc9a21f3dae9b825734734ea7a
Kevin Sieverding <kevin.sieverding@protonmail.com>
2022-05-27 21:05:54 +0200

    feat: extend simulator to also simulate machine data

--- simulator-app/index.js
+++ simulator-app/index.js
@@ -11,7 +11,7 @@ const kafka = new Kafka({
 const producer = kafka.producer();
 
 // Send tracking message to Kafka
-async function sendTrackingMessage(data) {
+async function sendMessage(data) {
   //Ensure the producer is connected
   await producer.connect();
 
@@ -23,13 +23,84 @@ async function sendTrackingMessage(data) {
     ],
   });
 
-  console.log("Send result:", result);
+  // console.log("Send result:", result);
   return result;
 }
 
+missions = ["sts-1", "sts-2", "sts-3", "sts-4"];
+
 setInterval(() => {
-  sendTrackingMessage({
-    msg: "Hello there!",
+  sendMessage({
+    mission: missions[Math.floor(Math.random() * (missions.length - 1))],
     timestamp: Math.floor(new Date() / 1000),
   });
 }, 1000);
+
+const temperatureTopic = "de.kevinsieverding.supervizor.temperature";
+const minTemp = 30;
+const maxTemp = 80;
+const tempInterval = 1000;
+
+setInterval(() => {
+  producer.connect();
+  producer.send({
+    topic: temperatureTopic,
+    messages: [
+      {
+        value: JSON.stringify({
+          temperature: (Math.random() * (maxTemp - minTemp) + minTemp).toFixed(
+            3,
+          ),
+          timestamp: Math.floor(new Date() / 1000),
+        }),
+      },
+    ],
+  });
+}, tempInterval);
+
+const pressureTopic = "de.kevinsieverding.supervizor.pressure";
+const minPressure = 80;
+const maxPressure = 160;
+const pressureInterval = 100;
+
+setInterval(() => {
+  producer.connect();
+  producer.send({
+    topic: pressureTopic,
+    messages: [
+      {
+        value: JSON.stringify({
+          pressure: (Math.random() * (maxPressure - minPressure) + minPressure)
+            .toFixed(
+              2,
+            ),
+          timestamp: Math.floor(new Date() / 1000),
+        }),
+      },
+    ],
+  });
+}, pressureInterval);
+
+const vibrationTopic = "de.kevinsieverding.supervizor.vibration";
+const minVibration = 0.5;
+const maxVibration = 0.7;
+const vibrationInterval = 1000;
+
+setInterval(() => {
+  producer.connect();
+  producer.send({
+    topic: vibrationTopic,
+    messages: [
+      {
+        value: JSON.stringify({
+          pressure:
+            (Math.random() * (maxVibration - minVibration) + minVibration)
+              .toFixed(
+                3,
+              ),
+          timestamp: Math.floor(new Date() / 1000),
+        }),
+      },
+    ],
+  });
+}, vibrationInterval);
