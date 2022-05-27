ae990d064f24f1faf7a04fa19f3f8b4e929ed544
Kevin Sieverding <kevin.sieverding@protonmail.com>
2022-05-27 16:27:40 +0200

    feat: serve web-app on port 8080

--- web-app/index.js
+++ web-app/index.js
@@ -2,8 +2,10 @@
 
 import { serve } from "https://deno.land/std@0.141.0/http/server.ts";
 
+const port = 8080;
+
 function handler(req) {
-  return new Response("Hello, World!")
+  return new Response("Hello, World!");
 }
 
-serve(handler)
+await serve(handler, { port });
